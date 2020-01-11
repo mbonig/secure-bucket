db2aeda2b1dceed27b33c2f9b1a528dafc7e21e4
Matthew Bonig <matthew.bonig@gmail.com>
2020-01-11 10:27:04 -0700

    test with encryption now

--- lib/index.test.ts
+++ lib/index.test.ts
@@ -2,12 +2,22 @@ import {SecureBucket} from "../lib/index";
 import {App, Stack} from "@aws-cdk/core";
 import {expect as expectCDK, haveResource} from '@aws-cdk/assert';
 
-test('Has one Bucket', () => {
+test('Has one encrypted Bucket', () => {
     const mockApp = new App();
     const stack = new Stack(mockApp, 'testing-stack');
 
     new SecureBucket(stack, 'testing', {});
 
-    expectCDK(stack).to(haveResource("AWS::S3::Bucket"));
+    expectCDK(stack).to(haveResource("AWS::S3::Bucket", {
+        "BucketEncryption": {
+            "ServerSideEncryptionConfiguration": [
+                {
+                    "ServerSideEncryptionByDefault": {
+                        "SSEAlgorithm": "aws:kms"
+                    }
+                }
+            ]
+        }
+    }));
 
 });
--- lib/index.ts
+++ lib/index.ts
@@ -1,12 +1,12 @@
 import {Construct} from "@aws-cdk/core";
-import {Bucket, BucketProps} from '@aws-cdk/aws-s3'
+import {Bucket, BucketEncryption, BucketProps} from '@aws-cdk/aws-s3'
 
 export class SecureBucket extends Construct {
 
     constructor(scope: Construct, id: string, props: BucketProps) {
         super(scope, id);
 
-        new Bucket(this, `${id}-bucket`, {...props});
+        new Bucket(this, `${id}-bucket`, {...props, encryption: BucketEncryption.KMS_MANAGED});
 
     }
 }
