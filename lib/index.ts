d79197c5a41be451026680a2f758f2dfab983737
Matthew Bonig <matthew.bonig@gmail.com>
2020-01-12 13:15:07 -0700

    removing the watch scipts since they're not working right now

--- lib/index.ts
+++ lib/index.ts
@@ -6,11 +6,12 @@ export class SecureBucket extends Construct {
     constructor(scope: Construct, id: string, props?: BucketProps) {
         super(scope, id);
 
-        let newProps: BucketProps = {...props};
-        if (!props || props?.encryption === undefined || props?.encryption === BucketEncryption.UNENCRYPTED) {
-            // @ts-ignore TS2540
-            newProps.encryption = BucketEncryption.KMS_MANAGED;
-        }
+        let newProps: BucketProps = {
+            ...props,
+            encryption: props && props.encryption && props.encryption != BucketEncryption.UNENCRYPTED
+                ? props.encryption
+                : BucketEncryption.KMS_MANAGED
+        };
         new Bucket(this, `${id}-bucket`, newProps);
     }
 }
